e643105bb132284b7e7d122288086aab6a0b32e0
C6H2Cl2 <ma87kk26ml@gmail.com>
2016-10-15 23:29:59 +0900

    とりま@Modクラス 後は任せた

---
+++ src/main/java/antimattaermod/GTAddon/AMMGTAddonCore.java
@@ -0,0 +1,13 @@
+package antimattaermod.GTAddon;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ * @author C6H2Cl2
+ */
+@Mod(modid = AMMGTAddonCore.MOD_ID,dependencies = "required-after:AntiMatterModCore@[1.5,);required-after:Forge@[10.13.4.1558,)")
+public class AMMGTAddonCore {
+    public static final String MOD_ID = "AMMGTAddon";
+    public static final String MOD_NAME = "AntiMatterMod GregTech Addon";
+    public static final String MOD_VERSION = "1.0.0";
+}
