01067b6dd394aa8e87df452024aa4de730e43c75
Raiti <rusuke@t-net.ne.jp>
2016-10-16 14:56:50 +0900

    AMMCoreの読み込みに成功 モジュールの更新は git submodule update --remote をターミナルで実行

--- src/main/java/antimattaermod/GTAddon/AMMGTAddonCore.java
+++ src/main/java/antimattaermod/GTAddon/AMMGTAddonCore.java
@@ -5,7 +5,7 @@ import cpw.mods.fml.common.Mod;
 /**
  * @author C6H2Cl2
  */
-@Mod(modid = AMMGTAddonCore.MOD_ID,dependencies = "required-after:AntiMatterModCore@[1.5,);required-after:Forge@[10.13.4.1558,)")
+@Mod(modid = AMMGTAddonCore.MOD_ID,dependencies = "required-after:AntiMatterModCore@[1.0.0,);required-after:Forge@[10.13.4.1558,)")
 public class AMMGTAddonCore {
     public static final String MOD_ID = "AMMGTAddon";
     public static final String MOD_NAME = "AntiMatterMod GregTech Addon";
