0c4d718d25be6b72c1f9353b1867eabd9f076b08
Raiti <rusuke@t-net.ne.jp>
2016-10-16 23:36:39 +0900

    greg機械のレシピ登録のテスト（成功）

--- src/main/java/antimattaermod/GTAddon/AMMGTAddonCore.java
+++ src/main/java/antimattaermod/GTAddon/AMMGTAddonCore.java
@@ -1,6 +1,11 @@
 package antimattaermod.GTAddon;
 
+import antimattaermod.core.AntiMatterModRegistry;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import gregtech.api.enums.GT_Values;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
 
 /**
  * @author C6H2Cl2
@@ -10,4 +15,11 @@ public class AMMGTAddonCore {
     public static final String MOD_ID = "AMMGTAddon";
     public static final String MOD_NAME = "AntiMatterMod GregTech Addon";
     public static final String MOD_VERSION = "1.0.0";
+    
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+        GT_Values.RA.addForgeHammerRecipe(new ItemStack(AntiMatterModRegistry.wrench_01,1,0),
+                new ItemStack(AntiMatterModRegistry.hammer_01,1,0),40,32);
+    }
+    
 }
