b9805549034fa3a88c4998b003dbb7b7428a7712
kojin15 <kojin15@amigo2.ne.jp>
2016-10-17 18:39:31 +0900

    プレートのレシピ

--- src/main/java/antimattaermod/GTAddon/AMMGTAddonCore.java
+++ src/main/java/antimattaermod/GTAddon/AMMGTAddonCore.java
@@ -3,9 +3,10 @@ package antimattaermod.GTAddon;
 import antimattaermod.core.AntiMatterModRegistry;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 import gregtech.api.enums.GT_Values;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.oredict.OreDictionary;
+
 
 /**
  * @author C6H2Cl2
@@ -15,11 +16,16 @@ public class AMMGTAddonCore {
     public static final String MOD_ID = "AMMGTAddon";
     public static final String MOD_NAME = "AntiMatterMod GregTech Addon";
     public static final String MOD_VERSION = "1.0.0";
-    
+
     @Mod.EventHandler
     public void init(FMLInitializationEvent event){
-        GT_Values.RA.addForgeHammerRecipe(new ItemStack(AntiMatterModRegistry.wrench_01,1,0),
-                new ItemStack(AntiMatterModRegistry.hammer_01,1,0),40,32);
+
+        GT_Values.RA.addBenderRecipe(new ItemStack(AntiMatterModRegistry.ingot_01, 1, 1), new ItemStack(AntiMatterModRegistry.plate_01, 1, 1), 60, 32);
+        GT_Values.RA.addBenderRecipe(new ItemStack(AntiMatterModRegistry.ingot_01,1,2),new ItemStack(AntiMatterModRegistry.plate_01,1,2),60,32);
+        GT_Values.RA.addBenderRecipe(new ItemStack(AntiMatterModRegistry.ingot_01,1,3),new ItemStack(AntiMatterModRegistry.plate_01,1,3),60,32);
+
     }
-    
+
 }
+    
+
